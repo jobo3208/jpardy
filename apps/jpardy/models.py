36f6c8b0a840c9c1c3ed8e4385b94fa3a711057a
Joe Boyd <jbistheman@gmail.com>
2012-01-15 13:04:46 -0500

    Wrote Category and Question models for jpardy.

--- apps/jpardy/models.py
+++ apps/jpardy/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey(User, editable=False)
+    played = models.BooleanField(default=False, editable=False)
+
+class Question(models.Model):
+    question = models.TextField(max_length=200)
+    answer = models.TextField(max_length=200)
+    category = models.ForeignKey(Category, editable=False)
+    daily_double = models.BooleanField(default=False)
+
+    VALUE_CHOICES = (
+        (100, '100'),
+        (200, '200'),
+        (300, '300'),
+        (400, '400'),
+        (500, '500'),
+    )
+    value = models.PositiveSmallIntegerField(choices=VALUE_CHOICES)
