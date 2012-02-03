11ae577a81720a60b2de499f89aab7c4a0e2703a
Joe Boyd <jbistheman@gmail.com>
2012-02-03 01:24:24 -0500

    Play work continues

--- static/js/jpardy.js
+++ static/js/jpardy.js
@@ -1,37 +1,16 @@
 currentQuestion = null;
 
-test = null;
+// The button that activated this question.
+currentQuestionSource = null;
 
 $(document).ready(function(){
     $('#question_area').hide();
-
-    player_table = $('<table/>').appendTo($('#question_area #who'));
-
-    for (player in game.players) {
-        row = $('<tr/>').appendTo(player_table);
-
-        $('<td/>').appendTo(row)
-            .text(game.players[player].username);
-
-        correct_td = $('<td/>').appendTo(row);
-        $('<button/>').appendTo(correct_td)
-            .addClass('correct_button')
-            .data('player', player);
-        
-        incorrect_td = $('<td/>').appendTo(row);
-        $('<button/>').appendTo(incorrect_td)
-            .addClass('incorrect_button')
-            .data('player', player);
-    }
-
-    $('#question_area #who button').click(function () {
-        $(this).attr('disabled', 'disabled');
-        $(this).removeClass().addClass('disabled_button');
-    });
 });
 
-displayQuestion = function(question) {
+displayQuestion = function(source, question) {
     currentQuestion = question;
+    currentQuestionSource = source;
+    resetAnswerArea();
     category_id = question.category;
     category_name = game.categories[category_id].name;
     $('#question_area #header_category').html(category_name);
@@ -41,21 +20,60 @@ displayQuestion = function(question) {
     $('#question_area').show('slow');
 };
 
+// Re-enables buttons and clears result text.
+resetAnswerArea = function() {
+    $('#answer_area')
+        .find('button:even')
+            .removeAttr('disabled')
+            .removeClass()
+            .addClass('correct_button')
+        .end()
+        .find('button:odd')
+            .removeAttr('disabled')
+            .removeClass()
+            .addClass('incorrect_button')
+        .end()
+        .find('tr')
+            .find('td:last')
+                .text('');
+};
+
 hideQuestion = function() {
     currentQuestion = null;
+    currentQuestionSource = null;
     $('#question_area').hide('slow');
 };
 
-correct = function(player_pk, question) {
-    adjustScore(player_pk, question.value);
+correct = function(source, player_pk) {
+    // Disable further button pushing and mark answer as correct.
+    $(source).closest('tr')
+        .find('button')
+            .attr('disabled', 'disabled')
+            .removeClass()
+            .addClass('disabled_button')
+        .end()
+        .find('td:last')
+            .text('correct');
+
+    $(currentQuestionSource).removeClass().addClass('asked_question');
+    hideQuestion();
 };
 
-incorrect = function(player_pk, question) {
-    adjustScore(player_pk, -question.value);
+incorrect = function(source, player_pk) {
+    // Disable further button pushing and mark answer as incorrect.
+    $(source).closest('tr')
+        .find('button')
+            .attr('disabled', 'disabled')
+            .removeClass()
+            .addClass('disabled_button')
+        .end()
+        .find('td:last')
+            .text('wrong');
 };
 
-nobodyGotIt = function(question) {
-    alert('nobody got it!');
+nobodyGotIt = function() {
+    $(currentQuestionSource).removeClass().addClass('asked_question');
+    hideQuestion();
 };
 
 adjustScore = function(player_pk, diff) {
