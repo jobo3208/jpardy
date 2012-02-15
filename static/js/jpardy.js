fe0fd62b554f26f57d30089e6911e0016c3a7381
Joe Boyd <jbistheman@gmail.com>
2012-02-15 03:10:14 -0500

    Daily doubles appear to be functioning correctly

--- static/js/jpardy.js
+++ static/js/jpardy.js
@@ -5,46 +5,47 @@ var currentQuestionNewResult = {};
 var currentDailyDoublePlayer = -1;
 var currentDailyDoubleWager = -1;
 
-selectQuestion = function(source, question_pk) {
+
+var selectQuestion = function(source, question_pk) {
     currentQuestion = game.questions[question_pk];
     currentQuestionSource = source;
+    displayCurrentQuestion();
+};
+
+
+var displayCurrentQuestion = function() {
+    hideAll();
+    resetAnswerRows();
 
     if (currentQuestion.asked) {
         displayAlreadyAskedQuestion();
     }
+    else if (currentQuestion.daily_double) {
+        displayDailyDouble();
+    }
     else {
-        displayCurrentQuestion();
+        displayRegularQuestion();
     }
 };
 
-displayCurrentQuestion = function() {
-    resetAnswerArea();
-//    hideAlreadyAskedQuestion();
-    hideAllFast();
 
-    var category_id = currentQuestion.category;
-    var category_name = game.categories[category_id].name;
-
-    if (currentQuestion.daily_double) {
-        $('#dd_area #header_category').html(category_name);
-        $('#dd_area #header_value').html(currentQuestion.value);
-        $('#dd_area').show('slow');
-    }
-    else {
-        $('#question_area #header_category').html(category_name);
-        $('#question_area #header_value').html(currentQuestion.value);
-        $('#question_area #question').html(currentQuestion.question);
-        $('#question_area #answer').html(currentQuestion.answer);
-        $('#question_area').show('slow');
-    }
+var hideAll = function(speed) {
+    $('#question_area').hide(speed);
+    $('#already_asked_area').hide(speed);
+    $('#dd_area').hide(speed);
 };
 
-displayAlreadyAskedQuestion = function() {
-//    hideCurrentQuestion();
-    hideAllFast();
 
+var resetAnswerRows = function() {
+    $('#answer_area tr').show();
+    $('#question_area #nobody').show();
+};
+
+
+var displayAlreadyAskedQuestion = function() {
     var category_id = currentQuestion.category;
     var category_name = game.categories[category_id].name;
+
     $('#already_asked_area #header_category').html(category_name);
     $('#already_asked_area #header_value').html(currentQuestion.value);
     $('#already_asked_area ul').html('');
@@ -57,139 +58,8 @@ displayAlreadyAskedQuestion = function() {
     $('#already_asked_area').show('slow');
 };
 
-resetAnswerArea = function() {
-    $('#answer_area')
-        .find('button:even')
-            .removeAttr('disabled')
-            .removeClass()
-            .addClass('correct_button')
-        .end()
-        .find('button:odd')
-            .removeAttr('disabled')
-            .removeClass()
-            .addClass('incorrect_button')
-        .end()
-        .find('tr')
-            .find('td:last')
-                .text('');
-};
 
-hideCurrentQuestion = function() {
-    $('#question_area').hide('slow');
-};
-
-hideAlreadyAskedQuestion = function() {
-    $('#already_asked_area').hide('slow');
-};
-
-hideAllFast = function() {
-    $('#question_area').hide();
-    $('#already_asked_area').hide();
-    $('#dd_area').hide();
-};
-
-correct = function(source, player_pk) {
-    // Disable further button pushing and mark answer as correct.
-    $(source).closest('tr')
-        .find('button')
-            .attr('disabled', 'disabled')
-            .removeClass()
-            .addClass('disabled_button')
-        .end()
-        .find('td:last')
-            .text('correct');
-
-    addResultToCurrentQuestion(player_pk, currentQuestion.value);
-    finishCurrentQuestion();
-};
-
-incorrect = function(source, player_pk) {
-    // Disable further button pushing and mark answer as incorrect.
-    $(source).closest('tr')
-        .find('button')
-            .attr('disabled', 'disabled')
-            .removeClass()
-            .addClass('disabled_button')
-        .end()
-        .find('td:last')
-            .text('wrong');
-
-    addResultToCurrentQuestion(player_pk, -currentQuestion.value);
-};
-
-cancelCurrentQuestion = function() {
-    hideCurrentQuestion();
-    resetCurrentQuestion();
-};
-
-cancelAlreadyAskedQuestion = function() {
-    hideAlreadyAskedQuestion();
-    resetCurrentQuestion();
-};
-
-redoAlreadyAskedQuestion = function() {
-    undoCurrentQuestionLastResult();
-    markCurrentQuestionAsUnasked();
-    currentQuestion.result = {};
-    sendCurrentQuestionToServer();
-    displayCurrentQuestion();
-};
-
-nobodyGotIt = function() {
-    finishCurrentQuestion();
-};
-
-finishCurrentQuestion = function() {
-    processCurrentQuestionResults();
-    currentQuestion.result = currentQuestionNewResult;
-    markCurrentQuestionAsAsked();
-    hideCurrentQuestion();
-    sendCurrentQuestionToServer();
-    resetCurrentQuestion();
-};
-
-sendCurrentQuestionToServer = function() {
-    $.ajax({
-        url: '/update_game/' + game.pk + '/',
-        type: 'POST',
-        data: $.toJSON(currentQuestion),
-        error: function() {
-            $('#ajax_status').html('failure!').fadeIn().delay(800).fadeOut();
-        },
-        success: function(data) {
-            $('#ajax_status').html('success').fadeIn().delay(800).fadeOut();
-        },
-        dataType: 'json'
-    });
-};
-
-addResultToCurrentQuestion = function(player_pk, score_change) {
-    currentQuestionNewResult[player_pk] = score_change;
-};
-
-markCurrentQuestionAsAsked = function() {
-    currentQuestion.asked = true;
-    $(currentQuestionSource).removeClass().addClass('asked_question');
-};
-
-markCurrentQuestionAsUnasked = function() {
-    currentQuestion.asked = false;
-    $(currentQuestionSource).removeClass().addClass('board_question');
-};
-
-processCurrentQuestionResults = function() {
-    for (var player_pk in currentQuestionNewResult) {
-        adjustScore(player_pk, currentQuestionNewResult[player_pk]);
-    }
-};
-
-undoCurrentQuestionLastResult = function() {
-    for (var player_pk in currentQuestion.result) {
-        adjustScore(player_pk, -currentQuestion.result[player_pk]);
-    }
-};
-
-getCurrentQuestionLastResultSummary = function() {
+var getCurrentQuestionLastResultSummary = function() {
     var results = [];
     var someoneGotIt = false;
 
@@ -215,30 +85,208 @@ getCurrentQuestionLastResultSummary = function() {
     return results;
 };
 
-resetCurrentQuestion = function() {
+
+var displayDailyDouble = function() {
+    resetDailyDouble();
+
+    var category_id = currentQuestion.category;
+    var category_name = game.categories[category_id].name;
+
+    $('#dd_area #header_category').html(category_name);
+    $('#dd_area #header_value').html(currentQuestion.value);
+    $('#dd_area').show('slow');
+};
+
+
+var resetDailyDouble = function() {
+    currentDailyDoublePlayer = -1;
+    currentDailyDoubleWager = -1;
+
+    $('#dd_area #wager_select').hide();
+    $('#dd_area #player_select').show();
+};
+
+
+var displayRegularQuestion = function() {
+    resetAnswerArea();
+
+    var category_id = currentQuestion.category;
+    var category_name = game.categories[category_id].name;
+
+    $('#question_area #header_category').html(category_name);
+    $('#question_area #header_value').html(currentQuestion.value);
+    $('#question_area #question').html(currentQuestion.question);
+    $('#question_area #answer').html(currentQuestion.answer);
+    $('#question_area').show('slow');
+};
+
+
+var resetAnswerArea = function() {
+    $('#answer_area')
+        .find('button:even')
+            .removeAttr('disabled')
+            .removeClass()
+            .addClass('correct_button')
+        .end()
+        .find('button:odd')
+            .removeAttr('disabled')
+            .removeClass()
+            .addClass('incorrect_button')
+        .end()
+        .find('tr')
+            .find('td:last')
+                .text('');
+};
+
+
+var correct = function(source, player_pk) {
+    // Disable further button pushing and mark answer as correct.
+    $(source).closest('tr')
+        .find('button')
+            .attr('disabled', 'disabled')
+            .removeClass()
+            .addClass('disabled_button')
+        .end()
+        .find('td:last')
+            .text('correct');
+
+    var value = (currentQuestion.daily_double) ? currentDailyDoubleWager : currentQuestion.value;
+    addResultToCurrentQuestion(player_pk, value);
+    finishCurrentQuestion();
+};
+
+
+var incorrect = function(source, player_pk) {
+    // Disable further button pushing and mark answer as incorrect.
+    $(source).closest('tr')
+        .find('button')
+            .attr('disabled', 'disabled')
+            .removeClass()
+            .addClass('disabled_button')
+        .end()
+        .find('td:last')
+            .text('wrong');
+
+    var value = (currentQuestion.daily_double) ? currentDailyDoubleWager : currentQuestion.value;
+    addResultToCurrentQuestion(player_pk, -value);
+
+    if (currentQuestion.daily_double) {
+        finishCurrentQuestion();
+    }
+};
+
+
+var addResultToCurrentQuestion = function(player_pk, score_change) {
+    currentQuestionNewResult[player_pk] = score_change;
+};
+
+
+var finishCurrentQuestion = function() {
+    processCurrentQuestionResults();
+    currentQuestion.result = currentQuestionNewResult;
+    markCurrentQuestionAsAsked();
+    hideAll('slow');
+    sendCurrentQuestionToServer();
+    resetCurrentQuestion();
+};
+
+
+var processCurrentQuestionResults = function() {
+    for (var player_pk in currentQuestionNewResult) {
+        adjustScore(player_pk, currentQuestionNewResult[player_pk]);
+    }
+};
+
+
+var adjustScore = function(player_pk, diff) {
+    game.players[player_pk].score += diff;
+    displayScore(player_pk, game.players[player_pk].score);
+};
+
+
+var displayScore = function(player_pk, score) {
+    $('#score_board #score' + player_pk).fadeOut().html(score).fadeIn();
+};
+
+
+var markCurrentQuestionAsAsked = function() {
+    currentQuestion.asked = true;
+    $(currentQuestionSource).removeClass().addClass('asked_question');
+};
+
+
+var sendCurrentQuestionToServer = function() {
+    $.ajax({
+        url: '/update_game/' + game.pk + '/',
+        type: 'POST',
+        data: $.toJSON(currentQuestion),
+        error: function() {
+            $('#ajax_status').html('failure!').fadeIn().delay(800).fadeOut();
+        },
+        success: function(data) {
+            $('#ajax_status').html('success').fadeIn().delay(800).fadeOut();
+        },
+        dataType: 'json'
+    });
+};
+
+
+var resetCurrentQuestion = function() {
     currentQuestion = null;
     currentQuestionSource = null;
     currentQuestionNewResult = {};
 };
 
-adjustScore = function(player_pk, diff) {
-    game.players[player_pk].score += diff;
-    displayScore(player_pk, game.players[player_pk].score);
+
+var cancelCurrentQuestion = function() {
+    hideAll();
+    resetCurrentQuestion();
 };
 
-displayScore = function(player_pk, score) {
-    $('#score_board #score' + player_pk).fadeOut().html(score).fadeIn();
+
+var redoAlreadyAskedQuestion = function() {
+    undoCurrentQuestionLastResult();
+    markCurrentQuestionAsUnasked();
+    currentQuestion.result = {};
+    sendCurrentQuestionToServer();
+    displayCurrentQuestion();
 };
 
-selectDailyDoublePlayer = function(player_pk) {
+
+var undoCurrentQuestionLastResult = function() {
+    for (var player_pk in currentQuestion.result) {
+        adjustScore(player_pk, -currentQuestion.result[player_pk]);
+    }
+};
+
+
+var markCurrentQuestionAsUnasked = function() {
+    currentQuestion.asked = false;
+    $(currentQuestionSource).removeClass().addClass('board_question');
+};
+
+
+var nobodyGotIt = function() {
+    finishCurrentQuestion();
+};
+
+
+var selectDailyDoublePlayer = function(player_pk) {
     currentDailyDoublePlayer = player_pk;
+    displayWagerSelect();
+};
+
+
+var displayWagerSelect = function(player_pk) {
     $('#dd_area #player_select').hide();
-    $('#dd_area #wager_select #chosen_player').html(game.players[player_pk].username);
+    $('#dd_area #wager_select #chosen_player').html(game.players[currentDailyDoublePlayer].username);
     $('#dd_area #wager_select').show();
 };
 
-selectDailyDoubleWager = function(wager) {
-    if (isNaN(parseInt(wager))) {
+
+var selectDailyDoubleWager = function(wager) {
+    wager = parseInt(wager);
+    if (isNaN(wager)) {
         $('#wager_select p.errors').html('That is not a valid wager.');        
     }
     else {
@@ -247,11 +295,25 @@ selectDailyDoubleWager = function(wager) {
         if (wager > maxWager) {
             $('#wager_select p.errors').html("You can't wager that much.");
         }
+        else if (wager < 0) {
+            $('#wager_select p.errors').html("Your wager cannot be negative.");
+        }
         else {
-            $('#wager_select p.errors').html("nice");
+            $('#wager_select p.errors').html("");
+            currentDailyDoubleWager = wager;
+            askDailyDoubleQuestion();
         }
     }
 };
+
+
+var askDailyDoubleQuestion = function() {
+    $('#answer_area tr:not(#answer_row_' + currentDailyDoublePlayer + ')').hide();
+    $('#question_area #nobody').hide();
+    hideAll();
+    displayRegularQuestion();
+};
+
 
 $(document).ready(function(){
     $('#question_area').hide();
